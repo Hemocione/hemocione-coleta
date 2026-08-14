666d97ebeb5259e0f0b770960f0df3d41170ef33
guim4dev <thiagoguimaraes.dev@gmail.com>
2026-08-13 22:10:44 -0300

    feat(collection-requests): endpoint de personalizacao de evento

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,6 +50,12 @@ export default defineNuxtConfig({
       process.env.HEMOCIONE_ID_JWT_SECRET_KEY ?? "secret",
     hemocioneIdIntegrationSecret:
       process.env.HEMOCIONE_ID_INTEGRATION_SECRET ?? "secret",
+    hemocioneDigitalEventUrl:
+      process.env.HEMOCIONE_DIGITAL_EVENT_URL ||
+      process.env.EVENTOS_HEMOCIONE ||
+      "https://eventos.d.hemocione.com.br",
+    coletaIntegrationSecret:
+      process.env.COLETA_INTEGRATION_SECRET ?? "secret",
   },
   nitro: {
     preset: "vercel", // Deploy no Vercel
